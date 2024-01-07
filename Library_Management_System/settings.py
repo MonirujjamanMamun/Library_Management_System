9df667f1318aef6119aa7c1b8280500ea341691a
unknown <monirujjamanmamun2357@gmail.com>
2024-01-07 23:32:46 +0600

    env file added

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import environ
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h*ikk%ql0%lf@=0!%#qjc^p7-*&aadge&*7h0*ilv1*tk5r4_('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mamunrahman2357@gmail.com'
-EMAIL_HOST_PASSWORD = 'uedd mnvw yidt soaq'
+EMAIL_HOST_USER = env("EMAIL_USER"),
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD"),

7566e8da04d948cd267cb3e9b86305ec31fff0de
unknown <monirujjamanmamun2357@gmail.com>
2024-01-09 12:22:33 +0600

    logout not working, solve this problem

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import UserRegistrationView, UserLoginView
+from . import views
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
+    # path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 class UserRegistrationView(FormView):
@@ -21,15 +22,39 @@ class UserRegistrationView(FormView):
         return super().form_valid(form)  # form_valid function call hobe jodi sob thik thake
 
 
+# class UserLoginView(LoginView):
+#     template_name = 'login.html'
+
+#     def get_success_url(self):
+#         return reverse_lazy('home')
+
 class UserLoginView(LoginView):
     template_name = 'login.html'
 
     def get_success_url(self):
         return reverse_lazy('home')
 
+    def form_valid(self, form):
+        messages.success(self.request, 'Logged in Successful')
+        return super().form_valid(form)
 
-class UserLogoutView(LogoutView):
-    def get_success_url(self):
-        if self.request.user.is_authenticated:
-            logout(self.request)
-        return reverse_lazy('home')
+    def form_invalid(self, form):
+        messages.success(self.request, 'Logged in information incorrect')
+        return super().form_invalid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type'] = 'Login'
+        return context
+
+# class UserLogoutView(LogoutView):
+#     def get_success_url(self):
+#         if self.request.user.is_authenticated:
+#             logout(self.request)
+#         return reverse_lazy('home')
+
+
+def user_logout(request):
+    messages.success(request, 'Log Out Successful.')
+    logout(request)
+    return redirect('login')
